b8bf685b128b689a1bbbbd224ddb4a42db3c9694
nini39-R <nini.gutierrez@est.iudigital.edu.co>
2025-02-28 15:00:55 +0000

    configuracion setup

---
+++ SRC/setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup,find_packages
+
+setup (
+    name= "nini_guitierrez",
+    version= "0.0.1",
+    author= "Nini gutierrez",
+    author_email= "nini.gutierrez@est.iudigital.edu.co",
+    description= "",
+    py_modules= ["actividad_1"],
+    install_requires= [
+        "pandas",
+        "matplotlib",
+        "requests"
+ ]
+ )
