3ad590c7a442ecaa3434a0c0c0d1e3f752682743
nini39-R <nini.gutierrez@est.iudigital.edu.co>
2025-03-12 20:53:10 +0000

    configuracion setup

--- setup.py
+++ setup.py
@@ -7,10 +7,16 @@ setup (
     author= "Nini gutierrez",
     author_email= "nini.gutierrez@est.iudigital.edu.co",
     description= "",
-    py_modules= ["actividad_1"],
+    py_modules= ["actividad_1 y 2"],
     install_requires= [
+       "kagglehub[pandas-datasets]>=0.3.8",
+        "matplotlib>=3.5.0",
+        "seaborn>=0.11.2",
         "pandas",
+        "numpy",
         "matplotlib",
+        "openpyxl",
         "requests"
+ 
  ]
  )
